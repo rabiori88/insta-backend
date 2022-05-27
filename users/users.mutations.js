50d7360fa4d8c3c9f468e5defef115d36594b5fd
rabiori <ctangtangbbang@gmaill.com>
2022-05-27 20:04:20 +0900

    create account

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,6 @@
 import client from "../client";
+import bcrypt from "bcrypt";
+import cli from "nodemon/lib/cli";
 
 export default {
   Mutation: {
@@ -21,6 +23,23 @@ export default {
         },
       });
       console.log(existingUser);
+
+      const uglyPassword = await bcrypt.hash(password, 10);
+      console.log(uglyPassword);
+      const user = client.user.create({
+        data: {
+          userName,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
+        },
+      });
+
+      return user;
+      //bcrypt.hash(password);
+      //bcrypt.compare();
+
       //hash password
       //save and return the user
     },
