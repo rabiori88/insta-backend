7e4005eebfd232ff1f74900f4705587e6d2c0619
rabiori <ctangtangbbang@gmaill.com>
2022-08-12 20:49:22 +0900

    4.13 Recap

--- users/users.utils.js
+++ users/users.utils.js
@@ -17,3 +17,24 @@ export const getUser = async (token) => {
     return null;
   }
 };
+
+export const protectResolver = (user) => {
+  if (!user) {
+    return {
+      ok: false,
+      error: "먼저 로그인 부터 하세요.",
+    };
+  }
+};
+
+export const protectedResolver =
+  (ourResolver) => (root, args, context, info) => {
+    if (!context.loggedInUser) {
+      return {
+        ok: false,
+        error: "제발 먼저 로그인 부터 하세요",
+      };
+    }
+
+    return ourResolver(root, args, context, info);
+  };
