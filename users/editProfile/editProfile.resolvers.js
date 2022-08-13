3ce7655b944592c43240e59e9f3184ceef433b0c
rabiori <ctangtangbbang@gmaill.com>
2022-08-13 18:50:17 +0900

    4.13 Recap

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,41 +1,42 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      console.log(loggedInUser);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, userName, email, password: newPassword },
+        { loggedInUser, protectResolver }
+      ) => {
+        let uglyPassword = null;
 
-      let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const ok = await client.user.update({
+          where: { id: loggedInUser.id },
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        });
 
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
+        if (ok) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "프로필 변경 실패",
+          };
+        }
       }
-      const ok = await client.user.update({
-        where: { id: loggedInUser.id },
-        data: {
-          firstName,
-          lastName,
-          userName,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-
-      if (ok) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "프로필 변경 실패",
-        };
-      }
-    },
+    ),
   },
 };
